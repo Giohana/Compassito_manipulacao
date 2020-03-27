cb2767e1ea477283e72e8217cc4956d61a6416fa
Giohana <giohana@outlook.com>
2020-03-27 13:46:54 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:manipulacao/http/webclient.dart';
+import 'package:manipulacao/models/Inputar.dart';
+
+void main() {
+  runApp(MyApp());
+  findAll().then((gerador) => print("nova lista: $gerador"));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manipulação'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          RaisedButton(
+              child: Text('Incluir'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Inputar();
+                }));
+              }),
+          RaisedButton(
+              child: Text('Editar'),
+              onPressed: () => debugPrint('pressionei o botão editar')),
+        ],
+      ),
+    );
+  }
+}
